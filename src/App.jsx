57ff94593ce63d76b123a6a1735e24295fb1a05f
ShahJamal6160 <shahj6160@gmail.com>
2025-05-10 22:04:05 +0600

    setup project with  react tailwind daisy

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-6xl text-center'>Vite + React</h1>
+
+      <div className='flex justify-center gap-4'>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App

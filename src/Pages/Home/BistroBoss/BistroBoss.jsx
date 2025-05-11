faf98d7d683d6c8b0450f1c1b6b338ca5493aee4
ShahJamal6160 <shahj6160@gmail.com>
2025-05-11 21:56:45 +0600

    added category & bistro section, using awesome react components

---
+++ src/Pages/Home/BistroBoss/BistroBoss.jsx
@@ -0,0 +1,18 @@
+import img12 from '../../../assets/home/chef-service.jpg'
+
+const BistroBoss = () => {
+    return (
+         <div className='my-24 relative'>
+            <img src={img12} alt="Chef Service" className="w-full h-auto" />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-20 rounded-lg shadow-lg text-center w-3/4">
+                <h1 className="text-4xl font-bold mb-4">BISTRO BOSS</h1>
+                <p className="text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id libero scelerisque lorem convallis rutrum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id libero scelerisque lorem convallis rutrum.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default BistroBoss;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
+import BistroBoss from '../BistroBoss/BistroBoss';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
+            <BistroBoss></BistroBoss>
         </div>
     );
 };
